2729fe542149ad1597ce264ca92f913c7d65df23
Daniel Park <op7773hons@gmail.com>
2016-11-18 01:04:19 +0900

    Improved JsonObject

--- src/main/java/com/danielpark/httpconnection/util/JsonObject.java
+++ src/main/java/com/danielpark/httpconnection/util/JsonObject.java
@@ -14,16 +14,6 @@ import org.json.JSONObject;
 public class JsonObject extends JSONObject {
 
     @Override
-    public Object get(String name) throws JSONException {
-        return super.get(FieldNamingPolicy.translateName(name));
-    }
-
-    @Override
-    public Object opt(String name) {
-        return super.opt(FieldNamingPolicy.translateName(name));
-    }
-
-    @Override
     public JSONObject put(String name, boolean value) throws JSONException {
         return super.put(FieldNamingPolicy.translateName(name), value);
     }
@@ -51,5 +41,35 @@ public class JsonObject extends JSONObject {
     @Override
     public JSONObject putOpt(String name, Object value) throws JSONException {
         return super.putOpt(FieldNamingPolicy.translateName(name), value);
+    }
+
+    @Override
+    public JSONObject accumulate(String name, Object value) throws JSONException {
+        return super.accumulate(FieldNamingPolicy.translateName(name), value);
+    }
+
+    @Override
+    public Object remove(String name) {
+        return super.remove(FieldNamingPolicy.translateName(name));
+    }
+
+    @Override
+    public boolean isNull(String name) {
+        return super.isNull(FieldNamingPolicy.translateName(name));
+    }
+
+    @Override
+    public boolean has(String name) {
+        return super.has(FieldNamingPolicy.translateName(name));
+    }
+
+    @Override
+    public Object get(String name) throws JSONException {
+        return super.get(FieldNamingPolicy.translateName(name));
+    }
+
+    @Override
+    public Object opt(String name) {
+        return super.opt(FieldNamingPolicy.translateName(name));
     }
 }

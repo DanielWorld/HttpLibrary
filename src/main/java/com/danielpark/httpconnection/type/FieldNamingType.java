8ea34e742565e9d7b97f7c41d62f201ba4760e7e
Daniel Park <op7773hons@gmail.com>
2016-11-17 23:11:12 +0900

    Fix uploading library issue

--- src/main/java/com/danielpark/httpconnection/type/FieldNamingType.java
+++ src/main/java/com/danielpark/httpconnection/type/FieldNamingType.java
@@ -13,10 +13,10 @@ public enum FieldNamingType {
      * Using this naming policy with response JSON will ensure that the field name is
      * unchanged.
      *
-     * <p>Here's a few examples of the form "Java Field Name" ---> "JSON Field Name":</p>
+     * <p>Here's a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
      * <ul>
-     *   <li>someFieldName ---> someFieldName</li>
-     *   <li>_someFieldName ---> _someFieldName</li>
+     *   <li>someFieldName ---&gt; someFieldName</li>
+     *   <li>_someFieldName ---&gt; _someFieldName</li>
      * </ul>
      */
     IDENTITY,
@@ -25,10 +25,10 @@ public enum FieldNamingType {
      * Using this naming policy with response JSON will ensure that the "letters" of
      * the Java field name is lowercase.
      *
-     * <p>Here's a few examples of the form "Java Field Name" ---> "JSON Field Name":</p>
+     * <p>Here's a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
      * <ul>
-     *   <li>someFieldName ---> somefieldname</li>
-     *   <li>_someFieldName ---> _somefieldname</li>
+     *   <li>someFieldName ---&gt; somefieldname</li>
+     *   <li>_someFieldName ---&gt; _somefieldname</li>
      * </ul>
      */
     LOWERCASE,
@@ -37,10 +37,10 @@ public enum FieldNamingType {
      * Using this naming policy with response JSON will ensure that the "letters" of
      * the Java field name is capitalized.
      *
-     * <p>Here's a few examples of the form "Java Field Name" ---> "JSON Field Name":</p>
+     * <p>Here's a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
      * <ul>
-     *   <li>someFieldName ---> SOMEFIELDNAME</li>
-     *   <li>_someFieldName ---> _SOMEFIELDNAME</li>
+     *   <li>someFieldName ---&gt; SOMEFIELDNAME</li>
+     *   <li>_someFieldName ---&gt; _SOMEFIELDNAME</li>
      * </ul>
      */
     UPPERCASE,
@@ -49,10 +49,10 @@ public enum FieldNamingType {
      * Using this naming policy with response JSON will ensure that the first "letter" of
      * the Java field name is capitalized.
      *
-     * <p>Here's a few examples of the form "Java Field Name" ---> "JSON Field Name":</p>
+     * <p>Here's a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
      * <ul>
-     *   <li>someFieldName ---> SomeFieldName</li>
-     *   <li>_someFieldName ---> _SomeFieldName</li>
+     *   <li>someFieldName ---&gt; SomeFieldName</li>
+     *   <li>_someFieldName ---&gt; _SomeFieldName</li>
      * </ul>
      */
     UPPER_CAMEL_CASE,
@@ -61,10 +61,10 @@ public enum FieldNamingType {
      * Using this naming policy with response JSON will ensure that the first "letter" of
      * the Java field name is capitalized and the words will be separated by a space.
      *
-     * <p>Here's a few examples of the form "Java Field Name" ---> "JSON Field Name":</p>
+     * <p>Here's a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
      * <ul>
-     *   <li>someFieldName ---> Some Field Name</li>
-     *   <li>_someFieldName ---> _Some Field Name</li>
+     *   <li>someFieldName ---&gt; Some Field Name</li>
+     *   <li>_someFieldName ---&gt; _Some Field Name</li>
      * </ul>
      */
     UPPER_CAMEL_CASE_WITH_SPACES,
@@ -73,10 +73,10 @@ public enum FieldNamingType {
      * Using this naming policy with response JSON will ensure that the first "letter" of
      * the Java field name is lowercase.
      *
-     * <p>Here's a few examples of the form "Java Field Name" ---> "JSON Field Name":</p>
+     * <p>Here's a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
      * <ul>
-     *   <li>SomeFieldName ---> someFieldName</li>
-     *   <li>_SomeFieldName ---> _someFieldName</li>
+     *   <li>SomeFieldName ---&gt; someFieldName</li>
+     *   <li>_SomeFieldName ---&gt; _someFieldName</li>
      * </ul>
      */
     LOWER_CAMER_CASE,
@@ -85,12 +85,12 @@ public enum FieldNamingType {
      * Using this naming policy with response JSON will modify the Java Field name from its camel cased
      * form to a lower case field name where each word is separated by an underscore (_).
      *
-     * <p>Here's a few examples of the form "Java Field Name" ---> "JSON Field Name":</p>
+     * <p>Here's a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
      * <ul>
-     *   <li>someFieldName ---> some_field_name</li>
-     *   <li>_someFieldName ---> _some_field_name</li>
-     *   <li>aStringField ---> a_string_field</li>
-     *   <li>aURL ---> a_u_r_l</li>
+     *   <li>someFieldName ---&gt; some_field_name</li>
+     *   <li>_someFieldName ---&gt; _some_field_name</li>
+     *   <li>aStringField ---&gt; a_string_field</li>
+     *   <li>aURL ---&gt; a_u_r_l</li>
      * </ul>
      */
     LOWER_CASE_WITH_UNDERSCORES,
@@ -99,12 +99,12 @@ public enum FieldNamingType {
      * Using this naming policy with response JSON will modify the Java Field name from its camel cased
      * form to a lower case field name where each word is separated by a dash (-).
      *
-     * <p>Here's a few examples of the form "Java Field Name" ---> "JSON Field Name":</p>
+     * <p>Here's a few examples of the form "Java Field Name" ---&gt; "JSON Field Name":</p>
      * <ul>
-     *   <li>someFieldName ---> some-field-name</li>
-     *   <li>_someFieldName ---> _some-field-name</li>
-     *   <li>aStringField ---> a-string-field</li>
-     *   <li>aURL ---> a-u-r-l</li>
+     *   <li>someFieldName ---&gt; some-field-name</li>
+     *   <li>_someFieldName ---&gt; _some-field-name</li>
+     *   <li>aStringField ---&gt; a-string-field</li>
+     *   <li>aURL ---&gt; a-u-r-l</li>
      * </ul>
      */
     LOWER_CASE_WITH_DASHES,
